a3bdfb4815ee74a7d090cec0752ec49067dfcf32
Jonathan <jkendler14@gmail.com>
2014-02-21 16:57:56 +0100

    Update

--- utestPlanner/utestPlanner.py
+++ utestPlanner/utestPlanner.py
@@ -35,8 +35,8 @@ class Project:
 
 ####
 class Task:
-    price = 0 # in USD
-    duration = 0 # in working days
+    price = 0  # in USD
+    duration = 0  # in working days
 
     def __init__(self):
         print "Adding task"
@@ -50,59 +50,56 @@ class Task:
 
 ####
 class UsabilityTest(Task):
-    def __init__(self, include_training=False, user_groups=[], locations=[], prepare_memo = False, report_format = "PPT", submit_to_IRB=False, submit_to_FDA=False, recruitExtra = True, conductPilot = False):
-        self.includeTraining = include_training
-        self.userGroups = user_groups
+    def __init__(self, include_training=False, user_groups=[], locations=[], prepare_memo = False, report_format = "PPT", submit_to_IRB=False, submit_to_FDA=False, recruit_extra = True, conduct_pilot = False):
+        self.include_training = include_training
+        self.user_groups = user_groups
         self.locations = locations
-        self.prepareMemo = prepare_memo
-        self.reportFormat = report_format
-        self.submitToIRB = submit_to_IRB
-        self.submitToFDA = submit_to_FDA
-        self.recruitExtra = recruitExtra
-        self.conductPilot = conductPilot
+        self.prepare_memo = prepare_memo
+        self.report_format = report_format
+        self.submit_to_IRB = submit_to_IRB
+        self.submit_to_FDA = submit_to_FDA
+        self.recruit_extra = recruit_extra
+        self.conduct_pilot = conduct_pilot
 
-    def calculateExpenses(self):
+    def calculate_expenses(self):
         self.expenses = 0
-        pass
 
-    def calculateLabor(self):
-        self.laborPrice = 0
-        pass
+    def calculate_labor(self):
+        self.labor_price = 0
 
-
-    def addUserGroup(self, userGroup):
-        self.userGroups.append(userGroup)
+    def add_user_group(self, user_group):
+        self.user_groups.append(user_group)
 
 ####
 class FormativeUsabilityTest(UsabilityTest):
-    def whatAmI(self):
+    def what_am_I(self):
         print "Adding formative usability test"
 
 class UserGroup:
-    def __init__(self, name, sessionDuration, numParticipants, compensation, recruitingRate):
+    def __init__(self, name, session_duration, num_participants, compensation, recruiting_rate):
         self.name = name
-        self.numParticipants = numParticipants
-        self.compensation = compensation #in usd
-        self.recruitingRate = recruitingRate #in usd
-        self.sessionDuration = sessionDuration # in minutes
+        self.num_participants = num_participants
+        self.compensation = compensation  # in usd
+        self.recruiting_rate = recruiting_rate  # in usd
+        self.session_duration = session_duration  # in minutes
         print "Added user group"
 
 class Location:
-    def __init__(self,city, state="MA", country="US", type="usability lab", pricePerDay=500):
+    def __init__(self, city, state="MA", country="US", type="usability lab", price_per_day=500):
         self.city = city
         self.country = country
         self.state = state
         self.type = type
-        self.pricePerDay = pricePerDay
+        self.price_per_day = price_per_day
     def __repr__(self):
-        return "LocationObj: " + " ".join([self.city, self.country, self.type, str(self.pricePerDay)])
+        return "LocationObj: " + " ".join([self.city, self.country, self.type, str(self.price_per_day)])
 
 usabilityTest = FormativeUsabilityTest()
 
 concordLab = Location("Concord")
-schlesinger = Location("Boston", type="market research facility", pricePerDay=1500)
+schlesinger = Location("Boston", type="market research facility", price_per_day=1500)
 
-labs = []
+labs = [ ]
 
 labs.append(concordLab)
 labs.append(schlesinger)
