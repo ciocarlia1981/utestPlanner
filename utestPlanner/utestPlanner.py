4b7fbc64cfae5329433b5a652248081a208eceff
Jonathan <jkendler14@gmail.com>
2014-02-22 19:12:08 +0100

    Update

--- utestPlanner/utestPlanner.py
+++ utestPlanner/utestPlanner.py
@@ -50,7 +50,7 @@ class Task:
 
 ####
 class UsabilityTest(Task):
-    def __init__(self, include_training=False, user_groups=[], locations=[], prepare_memo = False, report_format = "PPT", submit_to_IRB=False, submit_to_FDA=False, recruit_extra = True, conduct_pilot = False):
+    def __init__(self, include_training=False, user_groups=[], locations=[], team_members=[], prepare_memo = False, report_format = "PPT", submit_to_IRB=False, submit_to_FDA=False, recruit_extra = True, conduct_pilot = False):
         self.include_training = include_training
         self.user_groups = user_groups
         self.locations = locations
@@ -60,12 +60,27 @@ class UsabilityTest(Task):
         self.submit_to_FDA = submit_to_FDA
         self.recruit_extra = recruit_extra
         self.conduct_pilot = conduct_pilot
+        self.team_members = team_members
 
     def calculate_expenses(self):
         self.expenses = 0
 
     def calculate_labor(self):
         self.labor_price = 0
+        self.test_planning_price = calculate_test_planning(self)
+        self.test_conduct_price = self.calculate_test_conduct_labor()
+
+    def calculate_test_conduct_labor(self):
+        labor_price = 0
+        for group in self.user_groups:
+            for member in self.team_members:
+                duration_in_hours = group.session_duration / 60
+                labor_price = labor_price + (member.rate * group.num_participants * duration_in_hours)
+
+
+
+        print "The labor price for conducting the test is: ", labor_price
+        return labor_price
 
     def add_user_group(self, user_group):
         self.user_groups.append(user_group)
@@ -76,7 +91,7 @@ class FormativeUsabilityTest(UsabilityTest):
         print "Adding formative usability test"
 
 class UserGroup:
-    def __init__(self, name, session_duration, num_participants, compensation, recruiting_rate):
+    def __init__(self, name=None, session_duration=0, num_participants=8, compensation=0, recruiting_rate=0):
         self.name = name
         self.num_participants = num_participants
         self.compensation = compensation  # in usd
@@ -94,19 +109,27 @@ class Location:
     def __repr__(self):
         return "LocationObj: " + " ".join([self.city, self.country, self.type, str(self.price_per_day)])
 
-usabilityTest = FormativeUsabilityTest()
+class TeamMember:
+    def __init__(self, title="Human Factors Specialist", rate=125, name=None):
+        self.title = title
+        self.rate = rate
+        self.name = name
 
-concordLab = Location("Concord")
-schlesinger = Location("Boston", type="market research facility", price_per_day=1500)
 
-labs = [ ]
 
-labs.append(concordLab)
-labs.append(schlesinger)
 
-print labs
+def calculate_test_planning(usability_test):
+    pass
 
-for lab in labs:
-    print lab
+# sample usability test
 
-usabilityTest.get_price()
+nurses = UserGroup("Nurses",120,15,250)
+patients = UserGroup("Patients", 90, 15, 150)
+
+test_participants = [nurses]
+
+testing_team = [TeamMember("Managing Human Factors Specialist", 175, "Jon Smith"), TeamMember("Human Factors Specialist", 125, "Joana Smith")]
+
+sample_test = UsabilityTest(user_groups=test_participants,team_members=testing_team)
+sample_test.calculate_labor()
+
